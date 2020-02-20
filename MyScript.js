d40f9177ce0841cf9fd82c32bb91b38fb8f8a1ee
colonEndBracket <figdor@SLATE.localdomain>
2020-02-19 22:54:58 -0600

    fixed the element id string concatenation

--- MyScript.js
+++ MyScript.js
@@ -3,7 +3,28 @@ function refresh() {
 	console.log("Grabbing Value");
 	req.onreadystatechange = function () {
 		if (req.readyState == 4 && req.status == 200) {
-          		document.getElementById("demo").innerHTML = req.responseText;
+			var contents = req.responseText;
+			var tdata = contents.replace("\r","").split("\n");
+			var i;
+			var points = [];
+			console.log(tdata);
+			for (i=0; i<tdata.length; i++){
+				var p = tdata[i].split(" ");
+				points.push(p);
+			}
+			var j;
+			console.log(points);
+			for (i=0; i<points.length; i++){
+				console.log(i);
+				console.log(points[i].length);
+				for (j=0; j<points[i].length; j++){
+					console.log(i,j);
+					var genID = ["demo",i,j].join(""); 
+					console.log(genID);
+					document.getElementById( genID ).innerHTML = points[i][j];
+				}
+          			
+			}
         	}
       	}
 	req.open("GET", 'Bridge.txt', true);
@@ -12,7 +33,8 @@ function refresh() {
 
 function init() {
 	refresh()
-	var int = self.setInterval(function () {
-		refresh()
-	}, 1000);
+
+	//var some_var = self.setInterval(function () {
+	//	refresh()
+	//}, 1000);
 }
